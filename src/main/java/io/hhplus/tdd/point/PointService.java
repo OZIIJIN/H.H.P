ffed61f1fda7e63ceab2c272ae22b0529c1cedcc
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-03-28 01:06:27 +0900

    #1 feature : 포인트 충전 기능 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,18 +1,37 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.exception.ExceedMaximumPointException;
+import io.hhplus.tdd.exception.InvalidPointAmountException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
-    public PointService(UserPointTable userPointTable) {
+    public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
         this.userPointTable = userPointTable;
+        this.pointHistoryTable = pointHistoryTable;
     }
 
+
     public UserPoint charge(long userId, long amount) {
-        return null;
+        if (amount <= 0) {
+            throw new InvalidPointAmountException("포인트 충전 금앱은 0보다 커야 합니다.");
+        }
+
+        UserPoint userPoint = userPointTable.selectById(userId);
+
+        if (userPoint.point() + amount > 1000000L) {
+            throw new ExceedMaximumPointException("최대 포인트를 초과하셨습니다.");
+        }
+
+        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userPoint.id(), userPoint.point() + amount);
+        pointHistoryTable.insert(userPoint.id(), amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updatedUserPoint;
     }
 }
